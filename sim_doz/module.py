a07224677bbd87a6ec7b5e6ceaa5f26226940908
celestian <petr.celestian@gmail.com>
2019-04-22 08:59:06 +0200

    editor: Zoom
    It seems to be useful to have zoom feature to define how big elements
    will be.

--- sim_doz/module.py
+++ sim_doz/module.py
@@ -3,34 +3,39 @@
 import pyglet
 
 
+class Zoom:
+
+    def __init__(self, zoom_level):
+        coefficient = (zoom_level + 1) * 2
+        self.box_width = 2 * coefficient
+        self.box_heigth = 3 * coefficient
+
+
 class Grid:
 
-    def __init__(self):
+    def __init__(self, zoom):
 
         width = 800
         heigth = 600
 
         points = []
 
-        for w in range(0, width, 8):
-            for h in range(0, heigth, 12):
+        for w in range(0, width, zoom.box_width):
+            for h in range(0, heigth, zoom.box_heigth):
                 points.append(w)
                 points.append(h)
 
         number = int(len(points) / 2.0)
-        self.vertices = pyglet.graphics.vertex_list(number,
-                                                    ('v2i', points),
-                                                    ('c3B', [255, 0, 0] * number)
-                                                    )
-
-        print(number)
+        color = [255, 0, 0]
+        self.vertices = pyglet.graphics.vertex_list(
+            number, ('v2i', points), ('c3B', color * number))
 
 
 class EditorWindow(pyglet.window.Window):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.grid = Grid()
+        self.grid = Grid(Zoom(2))
 
     def on_draw(self):
         self.clear()
