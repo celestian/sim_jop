5e200dcd5e652ee9c3997132a03e96e0fae2d136
celestian <petr.celestian@gmail.com>
2019-04-27 17:19:32 +0200

    Packaging
    https://pypi.org/project/sim-jop/
    
    Resolves #27

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
-#!/usr/bin/env python3
+import setuptools
 
-from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(
-    name='sim_jop',
-    version='0.0.1',
-    packages=['sim_jop'],
+setuptools.setup(
+    name="sim_jop",
+    version="0.0.2",
+    author="celestian",
+    author_email="petr.celestian@gmail.com",
+    description="The Game of Railway Traffic Control",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/celestian/sim_jop",
+    packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': [
             'sim_jop = sim_jop.__main__:main'
         ]
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Games/Entertainment :: Simulation",
+        "Development Status :: 1 - Planning",
+    ],
 )
