f2f8933b5b7ba03e4f98eb01b04b500ffdfefcd3
celestian <petr.celestian@gmail.com>
2019-05-07 21:32:35 +0200

    editor: Window with grid
    Application draws main window with simple grid for better orientation.
    For closing the application press q.
    
    Resolves #36

--- sim_jop/__main__.py
+++ sim_jop/__main__.py
@@ -2,9 +2,7 @@
 
 """sim_jop
 Usage:
-  sim_jop schema prepare <area.yaml>
-  sim_jop schema check <area.yaml>
-  sim_jop schema show <area.yaml>
+  sim_jop editor <schema.yaml>
   sim_jop (-h | --help)
   sim_jop --version
 Options:
@@ -15,8 +13,7 @@ Options:
 from docopt import docopt
 import logging
 
-from sim_jop.railway.schema import Schema, prepare_schema
-from sim_jop.module import start_application
+from sim_jop.module import start_editor
 
 
 def main():
@@ -24,18 +21,12 @@ def main():
     Entry point
     """
 
-    args = docopt(__doc__, version='sim_jop 0.0.1')
+    args = docopt(__doc__, version='sim_jop 0.0.2')
 
     logging.basicConfig(level=logging.INFO)
 
-    if args['schema'] and args['prepare']:
-        prepare_schema(args['<area.yaml>'])
-
-    if args['schema'] and args['check']:
-        Schema(args['<area.yaml>'])
-
-    if args['schema'] and args['show']:
-        start_application(args)
+    if args['editor'] and args['<schema.yaml>']:
+        start_editor(args)
 
 
 if __name__ == '__main__':
--- sim_jop/gui/chart.py
+++ sim_jop/gui/chart.py
@@ -24,6 +24,7 @@ class ChartElement:
 
         return element_type
 
+
 class Chart:
 
     def __init__(self, schema):
