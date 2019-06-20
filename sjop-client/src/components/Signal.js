337ee8fcf437bc5485eaaca6241639fe67ceaa0b
David Rychlý <davidrychly2@gmail.com>
2019-06-20 20:52:15 +0200

    Track component coverted to 'line', cursor color change, signal reshape
    Reshape of signals to better fill cursor
    Change of cursor color to pink (same as Stanicar)
    Tracks converted from 'rectangle' shape to 'line'

--- sjop-client/src/components/Signal.js
+++ sjop-client/src/components/Signal.js
@@ -5,13 +5,26 @@ class Signal extends React.Component {
 
     // zoom 2, w 12, h 18
     render() {
+        var signal = this.props.signal === "red" ? "red" : this.props.signal === "green" ? "lime" : this.props.signal === "blue" ? "blue" : this.props.signal === "white" ? "white" : "gray";
         if (this.props.type === 1) {
             return (
                 <Line
                     y={1}
-                    points={this.props.dir === "r" ? [this.props.x * 12 - 9, this.props.y * 18 + 4, this.props.x * 12 - 3, this.props.y * 18 + 8, this.props.x * 12 - 9, this.props.y * 18 + 12, this.props.x * 12 - 10, this.props.y * 18 + 12, this.props.x * 12 - 10, this.props.y * 18 + 4] : this.props.dir === "l" ? [this.props.x * 12 - 3, this.props.y * 18 + 4, this.props.x * 12 - 9, this.props.y * 18 + 8, this.props.x * 12 - 3, this.props.y * 18 + 12, this.props.x * 12 - 2, this.props.y * 18 + 12, this.props.x * 12 - 2, this.props.y * 18 + 4] : ""}
-                    fill={this.props.signal === "red" ? "red" : this.props.signal === "green" ? "lime" : this.props.signal === "blue" ? "blue" : "gray"}
-                    stroke={this.props.signal === "red" ? "red" : this.props.signal === "green" ? "lime" : this.props.signal === "blue" ? "blue" : "gray"}
+                    points={this.props.dir === "r" ? [
+                        this.props.x * 12 - 9, this.props.y * 18 + 1,
+                        this.props.x * 12 - 2, this.props.y * 18 + 8,
+                        this.props.x * 12 - 9, this.props.y * 18 + 15,
+                        this.props.x * 12 - 10, this.props.y * 18 + 15,
+                        this.props.x * 12 - 10, this.props.y * 18 + 1
+                    ] : this.props.dir === "l" ? [
+                        this.props.x * 12 - 3, this.props.y * 18 + 1,
+                        this.props.x * 12 - 10, this.props.y * 18 + 8,
+                        this.props.x * 12 - 3, this.props.y * 18 + 15,
+                        this.props.x * 12 - 2, this.props.y * 18 + 15,
+                        this.props.x * 12 - 2, this.props.y * 18 + 1
+                    ] : ""}
+                    fill={signal}
+                    stroke={signal}
                     strokeWidth={2}
                     lineCap="square"
                     lineJoin="square"
@@ -22,8 +35,16 @@ class Signal extends React.Component {
             return (
                 <Line
                     y={1}
-                    points={this.props.dir === "r" ? [this.props.x * 12 - 9, this.props.y * 18 + 4, this.props.x * 12 - 3, this.props.y * 18 + 8, this.props.x * 12 - 9, this.props.y * 18 + 12] : this.props.dir === "l" ? [this.props.x * 12 - 3, this.props.y * 18 + 4, this.props.x * 12 - 9, this.props.y * 18 + 8, this.props.x * 12 - 3, this.props.y * 18 + 12] : ""}
-                    stroke={this.props.signal === "red" ? "red" : this.props.signal === "green" ? "lime" : this.props.signal === "blue" ? "blue" : "gray"}
+                    points={this.props.dir === "r" ? [
+                        this.props.x * 12 - 10, this.props.y * 18 + 1,
+                        this.props.x * 12 - 2, this.props.y * 18 + 8,
+                        this.props.x * 12 - 10, this.props.y * 18 + 15
+                    ] : this.props.dir === "l" ? [
+                        this.props.x * 12 - 2, this.props.y * 18 + 1,
+                        this.props.x * 12 - 10, this.props.y * 18 + 8,
+                        this.props.x * 12 - 2, this.props.y * 18 + 15
+                    ] : ""}
+                    stroke={signal}
                     strokeWidth={2}
                     lineCap="square"
                     lineJoin="square"
