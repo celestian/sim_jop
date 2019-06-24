f0faf0e9c4ad9654e745073530da7d51f0650bd1
David Rychlý <david.rychly@bootiq.io>
2019-06-24 11:50:51 +0200

    Added second option for turnout direction, updated initial JSON code
    * Added option to add turnout with points from 'left-top' to 'right-bottom'
    * Added default values for optional 'track' props
    *Updated preview JSON to show new turnout in the layout

--- sjop-client/src/components/Track.js
+++ sjop-client/src/components/Track.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Line} from 'react-konva';
+import {Line, Rect} from 'react-konva';
 
 class Track extends React.Component {
     // zoom 2, w 12, h 18
@@ -16,16 +16,46 @@ class Track extends React.Component {
                 />
             );
         } else if (this.props.type === 2) {
-            return (
-                <Line
-                  points={[
-                          this.props.x * 12, this.props.y * 18 + 9,
-                          this.props.x * 12 + 6, this.props.y * 18
-                      ]}
-                  strokeWidth={2}
-                  stroke="gray"
-                />
-            );
+            switch (this.props.dir) {
+                case 'lb-rt':
+                    return (
+                        <Line
+                        points={[
+                                this.props.x * 12, this.props.y * 18 + 9,
+                                this.props.x * 12 + 6, this.props.y * 18
+                            ]}
+                        strokeWidth={2}
+                        stroke="gray"
+                        />
+                    );
+                case 'lt-rb':
+                    return (
+                        <Line
+                        points={[
+                                this.props.x * 12, this.props.y * 18 + 9,
+                                this.props.x * 12 + 6, this.props.y * 18 + 18
+                            ]}
+                        strokeWidth={2}
+                        stroke="gray"
+                        />
+                    );
+                default:
+                    return (
+                        <Rect
+                        points={[
+                                this.props.x * 12, this.props.y * 18,
+                                this.props.x * 12, this.props.y * 18
+                            ]}
+                        x={this.props.x * 12}
+                        y={this.props.y * 18 + 4.5}
+                        width={6}
+                        height={9}
+                        strokeWidth={2}
+                        stroke="red"
+                        fill="pink"
+                        />
+                    );
+            }
         }
     }
 }
