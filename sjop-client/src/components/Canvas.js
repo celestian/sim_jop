5ec272a0b999a2e00e99a547daf04d7797ae4ccf
David Rychlý <david.rychly@bootiq.io>
2019-06-24 11:50:51 +0200

    Added second option for turnout direction, updated initial JSON code
    * Added option to add turnout with points from 'left-top' to 'right-bottom'
    * Added default values for optional 'track' props
    *Updated preview JSON to show new turnout in the layout

--- sjop-client/src/components/Canvas.js
+++ sjop-client/src/components/Canvas.js
@@ -62,7 +62,7 @@ class Canvas extends React.Component {
                 <Stage width={this.state.stageWidth} height={this.state.stageHeight} onMouseMove={this.getMouseCoords.bind(this.getMouseCoords)}>
                     <Layer>
                         {data['track'].map(i => (
-                            <Track key={i.key} x={i.x} y={i.y} length={i.len} type={i.type}/>
+                            <Track key={i.key} x={i.x} y={i.y} length={i.len ? i.len : 0} type={i.type} dir={i.dir ? i.dir : ''}/>
                         ))}
                         {data['signal'].map(i => (
                             <Signal key={i.key} x={i.x} y={i.y} type={i.type} dir={i.dir} signal={i.signal}/>
