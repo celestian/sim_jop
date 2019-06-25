29df2e508825b1e7c8c9651fa68484c9f9ccfc97
celestian <petr.celestian@gmail.com>
2019-06-25 11:49:11 +0200

    Junction component

---
+++ sjop-client/src/components/Junction.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Line, Rect} from 'react-konva';
+
+class Junction extends React.Component {
+    // zoom 2, w 12, h 18
+    render() {
+        return (
+            <Rect
+            points={[
+                    this.props.x * 12, this.props.y * 18,
+                    this.props.x * 12, this.props.y * 18
+                ]}
+            x={this.props.x * 12}
+            y={this.props.y * 18 + 4.5}
+            width={6}
+            height={9}
+            strokeWidth={2}
+            stroke="red"
+            fill="blue"
+            />
+        );
+    }
+}
+
+
+export default Junction;
