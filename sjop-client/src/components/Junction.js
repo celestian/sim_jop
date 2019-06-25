cd5bb05484beb75685f344946fe5aa1a3132ec17
celestian <petr.celestian@gmail.com>
2019-06-25 20:22:39 +0200

    First type of junction

--- sjop-client/src/components/Junction.js
+++ sjop-client/src/components/Junction.js
@@ -6,19 +6,24 @@ class Junction extends React.Component {
     render() {
         if (this.props.type === 1) {
             return (
-                <Rect
-                points={[
-                        this.props.x * 12, this.props.y * 18,
-                        this.props.x * 12, this.props.y * 18
-                    ]}
-                x={this.props.x * 12}
-                y={this.props.y * 18 + 4.5}
-                width={6}
-                height={9}
-                strokeWidth={2}
-                stroke="red"
-                fill="green"
-                />
+                <React.Fragment>
+                    <Line
+                      points={[
+                              this.props.x * 12, this.props.y * 18 + 9,
+                              this.props.x * 12 + 12, this.props.y * 18 + 9
+                          ]}
+                      strokeWidth={2}
+                      stroke="gray"
+                    />
+                    <Line
+                      points={[
+                              this.props.x * 12, this.props.y * 18 + 9,
+                              this.props.x * 12 + 6, this.props.y * 18 + 18
+                          ]}
+                      strokeWidth={2}
+                      stroke="gray"
+                    />
+                </React.Fragment>
             );
         } else {
             return (
@@ -39,5 +44,4 @@ class Junction extends React.Component {
         }
     }
 }
-
 export default Junction;
